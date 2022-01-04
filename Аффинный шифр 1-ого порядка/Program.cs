d17175eb54ec77dd6d2da0290821e56bbe24d356
aksen <aksen@DESKTOP-AORHCK0>
2022-01-04 15:33:10 +0200

    Добавьте файлы проекта.

---
+++ Аффинный шифр 1-ого порядка/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+namespace Athens_cipher
+{
+    
+    class Program
+    {
+        const string temp_rusABC = "абвгдежзийклмнопрстуфхцчшщъыьэюя_"; //m = 34
+        //const string temp_rusABC = "абвгдежзийклмнопрстуфхцчшщъыьэюя_,.?!";//м = 37
+        //const string temp_rusABC = "абвгдежзийклмнопрстуфхцчшщъыьэюя"; //м = 32
+        
+        const string chipher_word = "лвруияшащчыяэряшкга";
+        const string word_for_chipher = "Дима, помой посуду";
+        static int[] in_digit(char[] array)
+        {
+            int[] word = new int[array.Length];
+            char[] rus_ABC = temp_rusABC.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < rus_ABC.Length; j++)
+                {
+                    if (array[i] == rus_ABC[j])
+                    {
+                        word[i] = j;
+                    }
+                }
+            }
+            return word;
+        }
+        static char[] in_letter(int[] array)
+        {
+            char[] word = new char[array.Length];
+            char[] rus_ABC = temp_rusABC.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                word[i] = rus_ABC[array[i]];
+            }
+            return word;
+        }
+        static void Viviod(char [] array)
+        {
+            Console.WriteLine();
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+        static void Decipher2()
+        {
+            int S = 12;
+            int ABC = 33;  //не забыть поменять размер алфавита
+            int A1 = 26;
+
+            char[] word = chipher_word.ToCharArray();
+            int[] chipher_w;
+
+            chipher_w = in_digit(word);
+            
+            int[] arr_word = new int[word.Length];
+            for (int i = 0; i < arr_word.Length; i++)
+            {
+                int temp = A1 * (chipher_w[i] - S);
+                while (temp >= ABC || temp < 0)
+                {
+                    if (temp >= ABC)
+                    {
+                        temp -= ABC;
+                    }
+                    else
+                    {
+                        temp += ABC;
+                    }
+                }
+                arr_word[i] = temp;
+            }
+
+            Viviod(in_letter(arr_word));
+            
+        }
+        static void Decipher1() //для расшифровки с неизвестной частью ключа С
+        {
+            int A = 13;
+            int y = 32; //пробел 
+            int ABC = 34;         
+            int A1 = -13; //enter your A-1
+            int x = 34;
+
+            for (int k = 1; k <= x; k++)
+            {
+                int S = -A * k + y;
+                
+                while (S > ABC || S < 0)
+                {
+                    if (S > ABC)
+                    {
+                        S -= ABC;
+                    }
+                    else
+                    {
+                        S += ABC;
+                    }
+                }
+
+                
+                char[] word = chipher_word.ToCharArray();
+                int[] chipher_w = in_digit(word);
+                
+                int[] arr_word = new int[word.Length];
+                
+                for (int i = 0; i < arr_word.Length; i++)
+                {
+                    int temp = A1 * (chipher_w[i] - S);
+                    while (temp > ABC || temp < 0)
+                    {
+                        if (temp >= ABC)
+                        {
+                            temp -= ABC;
+                        }
+                        else
+                        {
+                            temp += ABC;
+                        }
+                    }
+                    arr_word[i] = temp;
+                }
+
+                char[] answer = in_letter(arr_word);
+                
+                Console.WriteLine();
+                for (int i = 0; i < answer.Length; i++)
+                {
+                   if(i == 0) Console.Write("["+ S + "]" + answer[i]);
+                   else
+                        Console.Write(answer[i]);
+                }
+            }
+           
+        }
+
+        static void Main(string[] args)
+        {
+            // Decipher2();
+        }
+        
+        static void Chipher_word()
+        {
+            int ABC = 34;
+            int A = 5;
+            int S = 25;
+
+            char[] symb_for_chipher = word_for_chipher.ToCharArray();
+            int[] word_in_digit = in_digit(symb_for_chipher);
+
+            int[] chipher_w = new int[word_in_digit.Length];
+            for (int i = 0; i < chipher_w.Length; i++)
+            {
+                int temp = A * word_in_digit[i] + S;
+                while (temp >= ABC || temp < 0)
+                {
+                    if (temp > ABC)
+                    {
+                        temp -= ABC;
+                    }
+                    else
+                    {
+                        temp += ABC;
+                    }
+                }
+                chipher_w[i] = temp;
+            }
+
+            Viviod(in_letter(chipher_w));
+
+
+
+        }
+    }
+}
---
+++ Аффинный шифр 2-ого порядка/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Athens_chipher
+{
+    public class Program
+    {
+        //const string temp_ABC = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ_,.?!";//м = 37
+        const string temp_ABC = "абвгдежзийклмнопрстуфхцчшщъыьэюя_";//м=33
+        //const string temp_ABC = "абвгдежзийклмнопрстуфхцчшщъыьэюя"; //м = 32
+        //const string temp_ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+        public static char[] rus_ABC = temp_ABC.ToCharArray();
+
+        const string word = "шкьлнъйтбт_ш";
+
+
+        static void Main(string[] args)
+        {
+            Dechipher();
+        }
+        static char[] in_letter(int[,] array, int length)
+        {
+            char[] word = new char[length];
+            //char[] rus_ABC = temp_ABC.ToCharArray();
+            int sch = 0;
+            for (int j = 0; j < (length/2); j++)
+            {
+                for(int i = 0; i < 2; i++)
+                {
+                    word[sch] = rus_ABC[array[i, j]];
+                    sch++;  
+                }
+                
+            }
+            return word;
+        }
+        static int[,] in_digit(char[] array, int length)
+        {
+            //кол-во данных зашифрованных букв /2
+            int[,] word = new int[2, length];
+            //char[] rus_ABC = temp_ABC.ToCharArray();
+            int l = 0;
+            for (int j = 0; j < length; j++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+
+                    for (int k = 0; k < rus_ABC.Length; k++)
+                    {
+                        if (array[l] == rus_ABC[k])
+                        {
+                            word[i, j] = k;
+                        }
+                    }
+                    l++;
+                }
+            }
+            
+            return word;
+        }
+        static void Dechipher()
+        {
+            char[] chipher_word = word.ToCharArray();
+           // char[] ABC = temp_ABC.ToCharArray();
+            int abc = rus_ABC.Length;
+            // ввести S
+            int[,] S = new int[,] { { 3 },  { 5 } };
+            // ввести матрицу А
+                int[,] A = new int[,] { { 4, 1 }, { 7, 2} };
+
+                int modA = A[0, 0] * A[1, 1] - A[0, 1] * A[1, 0];
+                Console.WriteLine("|A| = " + modA);
+                Console.WriteLine("Введите A^(-1) по модулю " + abc + ":");
+                int modA1 = Convert.ToInt32(Console.ReadLine());
+                int[,] A1 = new int[2, 2]
+                { {  ((modA1) * A[1, 1]),  modA1 * -(A[0, 1]) },
+                { modA1 * (-A[1, 0]), modA1 * A[0, 0]}
+                };
+                int length = chipher_word.Length / 2;
+                int[,] word_in_digit = in_digit(chipher_word, length);
+                //Y-S
+
+                for (int j = 0; j < length; j++)
+                {
+                    int x = 0;
+                    for (int i = 0; i < 2; i++)
+                    {
+                        word_in_digit[i, j] = word_in_digit[i, j] - S[x, 0];
+                        x++;
+                        while (word_in_digit[i, j] >= abc || word_in_digit[i, j] < 0)
+                        {
+                            if (word_in_digit[i, j] > abc)
+                            {
+                                word_in_digit[i, j] -= abc;
+                            }
+                            else
+                            {
+                                word_in_digit[i, j] += abc;
+                            }
+                        }
+                    }
+
+                }
+                int[,] new_word = new int[2, length];
+                //A1*(Y-S)
+                for (int j = 0; j < length; j++)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        if (i == 0)
+                        {
+                            new_word[i, j] = A1[0, 0] * word_in_digit[0, j] + A1[0, 1] * word_in_digit[1, j];
+                        }
+                        else
+                        {
+                            new_word[i, j] = A1[1, 0] * word_in_digit[0, j] + A1[1, 1] * word_in_digit[1, j];
+                        }
+                        while (new_word[i, j] >= abc || new_word[i, j] < 0)
+                        {
+                            if (new_word[i, j] >= abc)
+                            {
+                                new_word[i, j] -= abc;
+                            }
+                            else
+                            {
+                                new_word[i, j] += abc;
+                            }
+                        }
+                    }
+                }
+                char[] answer = in_letter(new_word, chipher_word.Length);
+                Console.WriteLine();
+                for (int i = 0; i < answer.Length; i++)
+                {
+                    Console.Write(answer[i]);
+                }
+            
+        }
+
+    }
+}
